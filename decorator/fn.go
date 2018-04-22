aa7135fc6861e9542f315b9fb23f3eefb382faf6
Neven Miculinic <neven.miculinic@gmail.com>
2018-04-22 19:36:57 +0200

    Updates

---
+++ decorator/fn.go
@@ -0,0 +1,13 @@
+package decorator
+
+import (
+	_ "context"
+	xx "context"
+)
+
+//go:generate decorator-gen -in=$GOFILE -out=gen-$GOFILE
+
+//@decorate:chan
+func Increase(a int, ctx xx.Context) (int,int){
+	return a + 1, 0
+}
